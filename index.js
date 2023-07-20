b45b2ed8a98a2d37ff8b1af68b8b19efa38166a9
Michael Pace <michael.g.pace@gmail.com>
2023-07-19 22:08:03 -0500

    root index matches appjs starter

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const PORT = process.env.PORT || 3002; //use strictt?
 const server = require('./server.js');
 const { sequelizeDatabase } = require('./src/auth/models');
 
-sequelizeDatabase
-  .sync()
-  .then(() => server.listen(PORT))
-  .catch((e) => console.error(e));
+// sequelizeDatabase
+//   .sync()
+//   .then(() => server.listen(PORT))
+//   .catch((e) => console.error(e));
+
+// sequelizeDatabase
+sequelizeDatabase.sync()
+  .then(() => {
+    server.listen(PORT, () => console.log('server up'));
+  }).catch(e => {
+    console.error('Could not start server', e.message);
+  });
